a25992d700c1e00c23d4ce2ed64cbf6f96d1e560
Foysal Ahamed <cfaion341@gmail.com>
2015-01-31 17:36:52 +0100

    playing around with wait()

--- fork2.c
+++ fork2.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/wait.h>
+
 /*
 fork2. c
 Illustrates fork, wait and exit return information
@@ -8,33 +13,38 @@ Last shell command should print the parent return value (15)
 
 */
 
-#include <stdio.h>
+int main() {
+	int n, status, cpid, self_pid, parent_pid;
 
-main() {
-int n, status, cpid;
+	printf("Parent pid = %d\n",  getpid());
 
-  printf("Parent pid = %d\n",  getpid());
+	n = fork();
 
-  n = fork();
+	if (n != 0) {
+		//  parent code
+		parent_pid = getppid();
+		self_pid = getpid();
+		printf ("Parent. PID: %d \t fork returned %d \t my parent is %d\n", self_pid, n, parent_pid);
 
-  if (n != 0) {
-//  parent code
-     printf ("I'm the parent with PID %d \t fork returned %d \t my parent is %d\n",
-              getpid(), n, getppid());
-     status = 0;
-     sleep(5);		// verify status of child
-     cpid = wait(&status);
-     printf("I received from my child %d this information %x\n", 
-             cpid, status);
-   }
-   else {		// child code
-        printf ("I'm the child with PID %d \t fork returned %d \t my parent is %d\n",
-	           getpid(), n, getppid());
-	      sleep(20);
-        printf("Child complete\n");
-        status=123;
-	      exit(4);
-     }
-     printf("Parent complete\n");
-     exit(15);
+		status = 100;
+		sleep(5);		// verify status of child
+
+		cpid = wait(&status);
+		printf("Parent. PID: %d received from: %d Status: %d\n", self_pid, cpid, status);
+	} else {		// child code
+		parent_pid = getppid();
+		self_pid = getpid();
+		printf ("Child. PID: %d \t fork returned %d \t my parent is %d\n", self_pid, n, parent_pid);
+
+		sleep(10);
+
+		printf("Child. Status: %d\n", status);
+		printf("Child. Complete\n");
+		char test[6] = "green";
+		status = 120;
+		exit(42);
+	}
+
+	printf("Parent. Complete\n");
+	exit(15);
 }
