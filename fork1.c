2c54ab529e220b9f03ee5758fe359283827f3af0
Foysal Ahamed <cfaion341@gmail.com>
2015-01-31 16:15:01 +0100

    address space separation between 2 forked methods understood

--- fork1.c
+++ fork1.c
@@ -1,27 +1,23 @@
-/*
-     fork1.c
-     Illustrates 
-      the use of separate process address spaces
-      with common inital values
-*/
+#include <stdio.h>
+#include <cstdlib>
+#include <unistd.h>
+
 
 int x,y; // global variables are not shared 
-int
-main()
-{ 
-int x,y; 
+int main() { 
+	int x,y; 
 
-x = 4;
-if (fork()){		// recall 0 in C is FALSE
-  y = 5;  	 // parent != 0
-  x = 15;
-  sleep(5);
-}
-else
-  y= -5;	// child
+	x = 4;
+	if (fork()){		// recall 0 in C is FALSE
+		y = 5;  	 // parent != 0
+		x = 15;
+		sleep(5);
+	} else {
+		y= -5;	// child
+	}
 
-printf(" x = %d y = %d process %d, PPID %d\n",
-          x, y, getpid(), getppid());
+	printf(" x = %d y = %d process %d, PPID %d\n",
+	          x, y, getpid(), getppid());
 
-exit(0);
+	exit(0);
 }
