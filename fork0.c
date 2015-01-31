bdc9aecc8d7bf004a75c22165e0ec146d19682d5
Foysal Ahamed <cfaion341@gmail.com>
2015-01-31 16:08:33 +0100

    ignoring binary builds and i understand fork() now! WOW

--- fork0.c
+++ fork0.c
@@ -1,24 +1,33 @@
-/*
-fork0. c
-Illustrates system call fork
+#include <stdio.h>
+#include <cstdlib>
+#include <unistd.h>
 
-*/
+int main() {
+	printf("I'm the parent, my PID is %d, my parent is process %d\n",
+	           getpid(), getppid());
 
-int
-main() {
+	int forkReturned = fork();
 
+	/*
+	                           p = fork();
+							assume no errors 
+							you now have two 
+							programs running 
+                         --------------------
+      if (p > 0) {                |            if (p == 0) {
+        printf("parent\n");       |              printf("child\n");
+        ...                       |              ...
+	*/
+	printf("the value of fork() is %d\n", forkReturned);
 
-printf("I'm the parent, my PID is %d, my parent is process %d\n",
-           getpid(), getppid());
+	if (forkReturned == 0){
+		// branch is for the parent
+		sleep (25);
+	}
+	printf("This sentence has been printed by process: %d my parent is process %d\n",
+	        getpid(), getppid());
 
-if (fork() == 0){
-	// branch is for the parent
-	sleep (25);
-}
-printf("This sentence has been printed by process: %d my parent is process %d\n",
-        getpid(), getppid());
+	// who is process number 1 ?? 
 
-// who is process number 1 ?? 
-
-exit(43);  // return 0;
+	exit(43);  // return 0;
 }
